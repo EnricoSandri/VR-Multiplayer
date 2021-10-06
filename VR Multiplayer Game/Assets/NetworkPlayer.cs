2fb2f786bea0cffffbca100172879f72d58c8746
Enrico <sandri.enrico86@gmail.com>
2021-10-06 11:45:30 +0100

    Added transformview for the avatar components and added is view
    Part 1 finish.

--- VR Multiplayer Game/Assets/NetworkPlayer.cs
+++ VR Multiplayer Game/Assets/NetworkPlayer.cs
@@ -3,21 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Photon.Pun;
 
-// this class applies the current positions of the body parts of the player avatar to the network player.
+// This class applies the current positions of the body parts of the player avatar to the network player.
+//additionally, it hides the network player instance to the player in control, this to avoid to see double avatar
 public class NetworkPlayer : MonoBehaviour
 {
+   private PhotonView photonView;
+   
    // set the networkplayer features 
    public Transform head;
    public Transform leftHand;
    public Transform rightHand;
 
+   private void Start()
+   {
+      photonView = GetComponent<PhotonView>();
+   }
+
    private void Update()
    {
-      // map each transform 
-      mapNetworkPlayerPosition(head,XRNode.Head);
-      mapNetworkPlayerPosition(leftHand,XRNode.LeftHand);
-      mapNetworkPlayerPosition(rightHand,XRNode.RightHand);
+      // check if the prefab is spawned by the player or someone else. if its the players, hide the avatar features
+      //Also, only the player can modify the transform. so it too is in the if();
+      if (photonView.IsMine)
+      {
+         head.gameObject.SetActive(false);
+         leftHand.gameObject.SetActive(false);
+         rightHand.gameObject.SetActive(false);
+         
+         // map each transform 
+         mapNetworkPlayerPosition(head,XRNode.Head);
+         mapNetworkPlayerPosition(leftHand,XRNode.LeftHand);
+         mapNetworkPlayerPosition(rightHand,XRNode.RightHand);
+      }
    }
 
    // get the inputs from the XR devices and apply it to the network player
